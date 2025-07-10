e5cf2622f6566045d4a02487cc8cc382a9ec3d12
rumon5h <mdrumonkhan48519@gmail.com>
2025-07-10 16:04:06 +0600

    updated

--- src/pages/UpdateBook.tsx
+++ src/pages/UpdateBook.tsx
@@ -1,28 +1,37 @@
+import Loading from '@/components/Loading';
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import React, { useState } from 'react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { useGetBookByIdQuery, useUpdateBookMutation } from '@/redux/api/baseApi';
 import { useForm, type FieldValues, type SubmitHandler, } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { useNavigate, useParams } from 'react-router';
 
 
-const book = {
-    "_id": "685684ffae705cec1192253c",
-    "title": "The Theory of Everything 3",
-    "author": "Stephen Hawking 2",
-    "genre": "SCIENCE",
-    "isbn": "978055338016323",
-    "description": "An overview of cosmology and black holes. 2",
-    "copies": 5,
-    "available": true,
-}
 
 const UpdateBook = () => {
+    const { id } = useParams<{ id: string }>();
+    const [updateBook] = useUpdateBookMutation();
+    const navigate = useNavigate();
+    const {data: book, isLoading} = useGetBookByIdQuery(id || "");
     const form = useForm();
 
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if(isLoading){
+        return <Loading />
+    }
+
+    const onSubmit: SubmitHandler<FieldValues> = async(data) => {
         console.log("Form submitted with data:", data);
 
+       const updated = await updateBook({ id: book.data._id, ...data });
 
+        if (updated.error) {
+            toast.error("Failed to update book.", { id: "form-error" });
+        } else if (updated.data?.success) {
+            toast.success("Book updated successfully!", { id: "form-success" });
+            navigate("/manage-books");
+        }
         form.reset();
     }
 
@@ -38,7 +47,7 @@ const UpdateBook = () => {
                             <FormItem>
                                 <FormLabel>Title</FormLabel>
                                 <FormControl>
-                                    <Input  {...field} value={field.value || book.title} />
+                                    <Input  {...field} value={field.value || book.data.title} />
                                 </FormControl>
 
                             </FormItem>
@@ -51,7 +60,7 @@ const UpdateBook = () => {
                             <FormItem>
                                 <FormLabel>Author</FormLabel>
                                 <FormControl>
-                                    <Input  {...field} value={field.value || book.author} />
+                                    <Input  {...field} value={field.value || book.data.author} />
                                 </FormControl>
 
                             </FormItem>
@@ -65,7 +74,7 @@ const UpdateBook = () => {
                             <FormItem>
                                 <FormLabel>Description</FormLabel>
                                 <FormControl>
-                                    <Input  {...field} value={field.value || book.description} />
+                                    <Input  {...field} value={field.value || book.data.description} />
                                 </FormControl>
 
                             </FormItem>
@@ -78,10 +87,22 @@ const UpdateBook = () => {
                         name="genre"
                         render={({ field }) => (
                             <FormItem>
-                                <FormLabel>Genre</FormLabel>
-                                <FormControl>
-                                    <Input  {...field} value={field.value || book.genre} />
-                                </FormControl>
+                                <FormLabel >Genre</FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value || book.data.genre}>
+                                    <FormControl>
+                                        <SelectTrigger className='w-full'>
+                                            <SelectValue placeholder="Select a Genre" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        <SelectItem value="FICTION">FICTION</SelectItem>
+                                        <SelectItem value="NON_FICTION">NON_FICTION</SelectItem>
+                                        <SelectItem value="SCIENCE">SCIENCE</SelectItem>
+                                        <SelectItem value="HISTORY">HISTORY</SelectItem>
+                                        <SelectItem value="BIOGRAPHY">BIOGRAPHY</SelectItem>
+                                        <SelectItem value="FANTASY">FANTASY</SelectItem>
+                                    </SelectContent>
+                                </Select>
 
                             </FormItem>
                         )}
@@ -94,7 +115,7 @@ const UpdateBook = () => {
                             <FormItem>
                                 <FormLabel>ISBN</FormLabel>
                                 <FormControl>
-                                    <Input  {...field} value={field.value || book.isbn} />
+                                    <Input  {...field} value={field.value || book.data.isbn} />
                                 </FormControl>
 
                             </FormItem>
@@ -107,7 +128,7 @@ const UpdateBook = () => {
                             <FormItem>
                                 <FormLabel>Copies</FormLabel>
                                 <FormControl>
-                                    <Input type="number" {...field} value={field.value || book.copies} />
+                                    <Input type="number" {...field} value={field.value || book.data.copies} />
                                 </FormControl>
 
                             </FormItem>
